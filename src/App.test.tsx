e7aab2fcc805052002777a6001c0405e5d0b2efe
Philip Miller <PhlipOutx@gmail.com>
2023-12-27 22:39:47 -0500

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders card number input field', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Card Number/);
+  expect(element).toBeInTheDocument();
+});
+
+test('valid card number sets is-valid class', () => {
+  const { container } = render(<App />);
+
+  const formControl = container.querySelector('#formCardNumber');
+  expect(formControl).toBeInTheDocument();
+  fireEvent.change(formControl!, {target: {value: '5454545454545454'}})
+
+  const primaryButton = screen.getByRole('button', { name: /Validate/i })
+  fireEvent.click(primaryButton)
+
+  expect(formControl).toHaveClass('is-valid')
+});
+
+test('invalid card number sets is-invalid class', () => {
+  const { container } = render(<App />);
+
+  const formControl = container.querySelector('#formCardNumber');
+  expect(formControl).toBeInTheDocument();
+  fireEvent.change(formControl!, {target: {value: '5454545454545450'}})
+
+  const primaryButton = screen.getByRole('button', { name: /Validate/i })
+  fireEvent.click(primaryButton)
+
+  expect(formControl).toHaveClass('is-invalid')
 });
